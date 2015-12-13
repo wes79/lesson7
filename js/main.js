7973a1d66c4bd920387a15e3c6d70f0b7a9b3ee4
wes79 <wesspasov@gmail.com>
2015-12-13 15:03:22 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-	document.getElementById("redDiv").addEventListener("mouseover",function(){
+	/*document.getElementById("redDiv").addEventListener("mouseover",function(){
 
 		this.style.marginTop = "50px";
 
@@ -11,9 +11,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 		this.style.marginTop = "10px";
 
-	});
+	});*/
 	var elements = document.querySelectorAll('input.testInput');
-	alert(elements);
+	
 	function makeItBigger(){ 
 	
 		
@@ -24,8 +24,35 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			}
 		
 	}
-	elements.addEventListener("focus",makeItBigger);
-	/*document.getElementById('button').addEventListener("click",makeItBigger);*/
+	for(i=0; i<elements.length; i++)
+		{
+
+			elements[i].addEventListener("focus",makeItBigger);
+		}
 	
+	function makeItSmaller(){ 
 	
+		
+		for(i=0; i<elements.length; i++)
+			{
+
+				elements[i].style.height= "16px";
+			}
+		
+	}
+	for(i=0; i<elements.length; i++)
+		{
+
+			elements[i].addEventListener("blur",makeItSmaller);
+		}
+
+	for(i=0; i<elements.length; i++)
+		{
+
+			elements[i].addEventListener("focus",function(){
+				
+				this.parentElement.style.backgroundColor="#000";
+			});
+		}
+
 });
